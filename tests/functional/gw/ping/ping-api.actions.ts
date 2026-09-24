ac6294509704782f2a1ca0fed524c07fe94ab896
kitz wang <mastahkitz@gmail.com>
2026-09-24 15:48:16 +0800

    refactor

--- tests/functional/gw/ping/ping-api.actions.ts
+++ tests/functional/gw/ping/ping-api.actions.ts
@@ -1,10 +1,10 @@
 import { APIRequestContext } from '@playwright/test';
 import { sendApiRequest } from '../../utils/api.utils';
 
-export async function sendPingRequest(request: APIRequestContext, apiKey: string) {
+export async function sendPingRequest(request: APIRequestContext, secret: string) {
   return sendApiRequest(request, {
     method: 'GET',
     url: 'api/v1/gw/ping',
-    headers: { 'x-api-key': apiKey },
+    headers: { 'x-api-key': secret },
   });
 }
